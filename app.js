ae7d56890fe236ca2ea7e098c1f18c5d701f3a70
Luis Silva <junior_mrv@live.com>
2020-10-07 00:35:42 -0300

    HTML/CSS

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express       = require('express');
+const exphbs        = require('express-handlebars');
 const app           = express();
+const path          = require('path');
 const db            = require('./db/connection');
 const bodyParser    = require('body-parser');
 
@@ -11,6 +13,13 @@ app.listen(PORT, function(){
 
 // body parses
 app.use(bodyParser.urlencoded({extended:false}));
+
+//handle bars
+app.set('views', path.join(_dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
 
 // db conexao
 db
