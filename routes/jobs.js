f729c0e8f90eb13779f9bcaf62f114485704f2b4
Luis Silva <junior_mrv@live.com>
2020-10-10 01:04:58 -0300

    Finalizando projeto, deixando o projeto responsivo

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,11 +2,25 @@ const express = require ('express');
 const router = express.Router();
 const Job = require('../models/Job');
 
+//ROTA DE TESTE
 router.get('/test', (req, res)=>{
     res.send('deu certo');
 
 })
+//DETALHE DA VAGA -> VIEW/1, VIEW/2
+router.get('/view/:id', (req,res)=> Job.findOne({
+     where: {id: req.params.id}
+    }).then(job =>{
+        res.render('view', {
+            job
+        });
+}).catch(err => console.log(err)));
 
+//FORM DA ROTA DE ENVIO
+router.get('/add', (req,res)=> {
+    res.render('add');
+})
+//ADD JOB VIA POST
 router.get('/add', (req,res)=> {
     res.render('add');
 })
